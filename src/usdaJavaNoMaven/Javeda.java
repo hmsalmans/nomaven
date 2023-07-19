351bb5bea5c805f319c4d339c1aed41f103b1700
Salman <hmsalmans1@gmail.com>
2023-07-19 05:13:03 +0500

    adding files

---
+++ src/usdaJavaNoMaven/Javeda.java
@@ -0,0 +1,28 @@
+package usdaJavaNoMaven;
+
+public class Javeda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		result(8,4);
+		
+		
+		
+		
+	}
+
+	
+	public static void result (int x, int y) {
+		int total = x + y;
+		System.out.println(total);
+	}
+	
+	
+	
+	
+	
+}
